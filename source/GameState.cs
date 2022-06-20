260defdd909213b466299b1a9433bb5edbec1c35
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2022-06-20 11:59:33 -0400

    fix for disable Buyback causing failure to start career

--- source/GameState.cs
+++ source/GameState.cs
@@ -10,9 +10,23 @@ namespace CustomShops
 {
     public class GameState
     {
-        public StarSystem CurrentSystem { get; internal set; }
+        private StarSystem _currentStarSystem;
+        public StarSystem CurrentSystem
+        {
+            get
+            {
+                if (_currentStarSystem == null)
+                {
+                    Control.Log($"Control.State.CurrentSystem was null. Setting to Control.Sim.CurSystem: {Sim.CurSystem.Name}");
+                    CurrentSystem = Sim.CurSystem;
+                }
+                return _currentStarSystem;
+            }
+            internal set => _currentStarSystem = value;
+        }
+
         public Sprite SystemShopSprite { get; internal set; }
         public Sprite BlacMarketSprite { get; internal set; }
         public SimGameState Sim { get; internal set; }
-}
+    }
 }
